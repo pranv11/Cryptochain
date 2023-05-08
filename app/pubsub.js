646aff30d8d547670643b0b9b146062a1431c8de
pranavpandey <pranavpandey2720@gmail.com>
2023-05-08 15:51:41 -0700

    final code

---
+++ app/pubsub.js
@@ -0,0 +1,76 @@
+const PubNub = require('pubnub');
+
+const credentials = {
+  publishKey: 'pub-c-08503790-f156-4f3c-a3b8-983c3b62a482',
+  subscribeKey: 'sub-c-edf87712-b83a-48d3-a126-b4b26e7bf1ef',
+  secretKey: 'sec-c-M2RhNGZiYWYtZDFjZi00OTQ4LThiNGQtOWFjZDQxMTliNTdm'
+};
+
+const CHANNELS = {
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
+  TRANSACTION: 'TRANSACTION'
+};
+
+class PubSub {
+  constructor({ blockchain, transactionPool }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+
+    this.pubnub = new PubNub(credentials);
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+    this.pubnub.addListener(this.listener());
+  }
+
+  handleMessage(channel, message) {
+    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+
+    const parsedMessage = JSON.parse(message);
+
+    switch(channel) {
+      case CHANNELS.BLOCKCHAIN:
+        this.blockchain.replaceChain(parsedMessage, true, () => {
+          this.transactionPool.clearBlockchainTransactions({
+             chain: parsedMessage
+          });
+        });
+        break;
+      case CHANNELS.TRANSACTION:
+        this.transactionPool.setTransaction(parsedMessage);
+        break;
+      default:
+        return;
+    }
+  }
+
+
+  listener() {
+    return {
+      message: messageObject => {
+        const { channel, message } = messageObject;
+
+        this.handleMessage(channel, message);
+      }
+    };
+  }
+
+  publish({ channel, message}) {
+    this.pubnub.publish({ channel, message });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction)
+    })
+  }
+}
+
+module.exports = PubSub;
